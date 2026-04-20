2a447c9139b32db3908d1e40b59c48f9cbede5d0
Urvi_2003 <103057392+USP2003@users.noreply.github.com>
2026-04-20 09:36:17 +0530

    Update config.java

--- src/Databaseconfig/config.java
+++ src/Databaseconfig/config.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class config {
     static String url="jdbc:mysql://localhost:3306/bookmymovies";
-    static  String username="root";
-    static String password="SwissVisa@2027";
+    static  String username="username";
+    static String password="password";
 
     public static Connection getconnection() throws SQLException {
         return DriverManager.getConnection(url,username,password);
